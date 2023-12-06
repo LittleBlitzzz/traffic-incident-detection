6708e063cfa6c1316195ac3764c96d67cbf2bf36
LittleBlitzzz <littleblitzzz@gmail.com>
2023-12-06 12:49:55 +0000

    feat : adding prompt testing API

--- flask-react/backend/api/model_api.py
+++ flask-react/backend/api/model_api.py
@@ -2,6 +2,8 @@ from flask import Flask, Blueprint, request
 
 import os
 import logging
+import yaml
+import pandas as pd
 logger = logging.getLogger()
 
 from traffic_llava.traffic_llava import PromptFramework, setup_model
@@ -16,25 +18,23 @@ def ask_llava():
   results = { "error_message": "" }
 
   if request.method == "POST":
-
     request_json = request.get_json()
     logger.debug("Ask LLaVA - request : " + str(request_json))
-    
-    full_img_path = request_json.get("full_img_path", None)
-    if full_img_path is None:
-      dataset_name = request_json.get("dataset_name", None)
-      video_name = request_json.get("video_name", None)
-      image_filename = request_json.get("image_filename", None)
 
+    full_img_path = request_json.get("full_img_path", None)
+    dataset_name = request_json.get("dataset_name", None)
+    video_name = request_json.get("video_name", None)
+    image_filename = request_json.get("image_filename", None)
+    prompt_framework_args = request_json.get("prompt_framework", None)
+    single_text_prompt = request_json.get("single_text_prompt", None)
+    save_path_name = request_json.get("save_path_name", "/content/test.txt")
+    
+    if full_img_path is None:
       if dataset_name is not None and video_name is not None and image_filename is not None:
         full_img_path = os.path.join(os.environ["datasets_path"], dataset_name, video_name, image_filename)
 
     logger.debug("Ask LLaVA - image path : " + full_img_path)
     if full_img_path is not None and os.path.exists(full_img_path):
-      prompt_framework_args = request_json.get("prompt_framework", None)
-      single_text_prompt = request_json.get("single_text_prompt", None)
-      save_path_name = request_json.get("save_path_name", "/content/test.txt")
-
       if prompt_framework_args is not None:
         prompt_framework = PromptFramework(**prompt_framework_args)
         model_output = prompt_framework.apply_on_image(model, full_img_path, save_path_name)
@@ -51,3 +51,40 @@ def ask_llava():
 
   return results
 
+@model_api.route("/test-on-annotated-data", methods=["POST"])
+def test_on_annotated_data():
+  results = { "error_message": "" }
+
+  if request.method == "POST":
+    request_json = request.get_json()
+    logger.debug("Test LLaVA - request : " + str(request_json))
+    
+    dataset_name = request_json.get("dataset_name", "extracted_frames")
+    prompt_framework_args = request_json.get("prompt_framework", None)
+    output_directory = os.path.join(os.environ["results_path"], request_json.get("save_directory", "test_llava"))
+    
+    annotation_path = request_json.get("annotation_path", os.path.join(os.environ["results_path"], "annotations_compiled_20231206122224.csv"))
+    annotation_df = pd.read_csv(annotation_path)
+    annotated_image_filepaths = annotation_df["imagepath"]
+
+    if prompt_framework_args is None:
+      results["error_message"] += "No prompt framework received!\n"
+    else:
+      test_results = []
+      results["test_results"] = test_results
+
+      for index, image_filepath in enumerate(annotated_image_filepaths):
+        full_img_path = os.path.join(os.environ["datasets_path"], dataset_name, image_filepath)
+
+        logger.debug("Ask LLaVA - image path : " + full_img_path)
+
+        prompt_framework = PromptFramework(**prompt_framework_args)
+        model_output = prompt_framework.apply_on_image(model, full_img_path, os.path.join(output_directory, "results_" + str(index) + ".txt"))
+        test_results.append(model_output)
+
+        logger.debug("LLaVA replies : " + str(model_output))
+
+      with open(os.path.join(output_directory, "results_total.yaml"), "w") as results_total_file:
+        yaml.dump(test_results, results_total_file)
+
+  return results
